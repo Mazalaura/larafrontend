320cf730450185ec4146b1d1c65fe9e06e121d34
Mazalaura <mazalaura@gmail.com>
2021-05-24 20:31:23 +0300

    05.24

---
+++ _js/app.js
@@ -0,0 +1,68 @@
+
+console.log('------>', 9+11);
+
+
+
+[1, 2, 8].forEach(val => console.log(val)); 
+
+console.log('A6 esu draugas Nr. 1', 15.478*18.89);
+
+var a, b, e, f;
+var d = 45;
+var t = 45;
+var g = 3;
+
+a = 1;
+console.log('d', d)
+d = 100;
+console.log('d', d)
+b = 1;
+b = d + 100;
+b = d + d - t + 25;
+
+console.log(a);
+console.log(b);
+
+b = d + d;
+console.log('+', d + t);
+
+console.log('-', d - t);
+
+console.log('*', d * t);
+
+console.log('/', d / t);
+
+console.log('**', d ** g);
+
+console.log('%', d % t);
+
+console.log('|++', d++);
+
+console.log('++', d);
+
+console.log('++|', ++t)
+
+console.log('++', t);
+
+console.log('g', --g);
+
+g = g + 7;
+
+g += 7;// g = g -7
+
+console.log('g+=', g);
+
+
+console.log('()', g + 6 * 2)
+
+document.querySelector('h2').innerHTML = g;
+
+document.querySelector('h2').style.color = 'red';
+var nn;
+var n = '7';
+var n2 = 7;
+nn = n + n2;
+n++;
+console.log(typeof n)
+console.log('n', nn)
+
